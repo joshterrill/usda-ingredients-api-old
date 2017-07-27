8903841dae253b18580c27f266b34e6ea9879773
Josh Terrill <joshterrill.dev@gmail.com>
2017-07-26 23:32:44 -0700

    added dotenv library and version output to GET /

--- src/api/index.js
+++ src/api/index.js
@@ -1,14 +1,16 @@
 const Router = require('express').Router;
+const version = require('../../package.json').version
+const env = require('dotenv').config();
 
 module.exports = () => {
   const api = Router();
 
   api.get('/', (req, res) => {  
-  	res.json({api: true});
+  	res.json({version});
   });
 
-  api.get('/api/:number', (req, res) => {
-    res.json({number: req.params.number});
+  api.get('/api/getUsdaUrl', (req, res) => {
+    res.json({url: process.env.USDA_URL});
   });
 
   return api;

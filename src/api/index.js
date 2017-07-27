e282573fbcaddf3ca159da9e5e4a99d4d457eb6e
Josh Terrill <joshterrill.dev@gmail.com>
2017-07-27 00:10:41 -0700

    added usda url to link so that the whole link is included in response

--- src/api/index.js
+++ src/api/index.js
@@ -20,9 +20,9 @@ module.exports = () => {
     request.get(`${process.env.USDA_URL}/ndb/search/list?qlookup=${upc}`, (err, response, body) => {
       if (err) res.json({err});
       let $ = cheerio.load(body.replace(/[\n\r\t]+/g, ''));
-      const link = $($($($('.list-left table tr')[1])).children().slice(2).html()).attr('href').replace(/[\n\r\t]+/g, '');
+      const link = process.env.USDA_URL + $($($($('.list-left table tr')[1])).children().slice(2).html()).attr('href').replace(/[\n\r\t]+/g, '');
       const text = $($($($('.list-left table tr')[1])).children().slice(2).html()).text().replace(/[\n\r\t]+/g, '');
-      request.get(`${process.env.USDA_URL}${link}`, (lErr, lRes, lBody) => {
+      request.get(link, (lErr, lRes, lBody) => {
         $ = cheerio.load(lBody.replace(/[\n\r\t]+/g, ''));      
         let ingredients = $('.container .row .col-md-12').slice(5).html().replace(/[\n\r\t]+/g, '').replace('<strong>Ingredients: </strong>', '').split('<span')[0].trim();
         if (ingredients.endsWith('.')) {

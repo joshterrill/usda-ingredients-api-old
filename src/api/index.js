d5507c40b96baecdd658ebf8012a528a820161c1
Josh Terrill <joshterrill.dev@gmail.com>
2017-07-27 00:00:36 -0700

    added a cheerio query selector for getting the result link and text

--- src/api/index.js
+++ src/api/index.js
@@ -2,6 +2,7 @@ const Router = require('express').Router;
 const version = require('../../package.json').version
 const env = require('dotenv').config();
 const request = require('request');
+const cheerio = require('cheerio');
 
 module.exports = () => {
   const api = Router();
@@ -17,11 +18,11 @@ module.exports = () => {
   api.get('/api/query', (req, res) => {
     const upc = req.query.upc;
     request.get(`${process.env.USDA_URL}/ndb/search/list?qlookup=${upc}`, (err, response, body) => {
-      if (err) {
-        console.log(err);
-        return;
-      }
-      res.json({body})
+      if (err) res.json({err});
+      let $ = cheerio.load(body.replace(/[\n\r\t]+/g, ''));
+      const link = $($($($('.list-left table tr')[1])).children().slice(2).html()).attr('href').replace(/[\n\r\t]+/g, '');
+      const text = $($($($('.list-left table tr')[1])).children().slice(2).html()).text().replace(/[\n\r\t]+/g, '');
+      res.json({link, text})
     });
   });
 
